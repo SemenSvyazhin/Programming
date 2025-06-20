2d6cef50f8b18baf4d2587ebb5c5720268e1f897
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 18:50:28 +0500

    Update Example2.java

--- src/lab9/task1/Example2.java
+++ src/lab9/task1/Example2.java
@@ -1,4 +1,15 @@
 package lab9.task1;
 
 public class Example2 {
+    public static void m(int x) {
+        int next_value = 2 * x + 1;
+        if (next_value < 20) {
+            m(next_value);
+        }
+        System.out.println("x=" + x);
+    }
+
+    public static void main(String[] args) {
+        m(0);
+    }
 }
