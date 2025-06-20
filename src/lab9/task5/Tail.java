693cfdb23f34227ec6fde7bb5744597bd3909387
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 19:01:14 +0500

    Update Tail.java

--- src/lab9/task5/Tail.java
+++ src/lab9/task5/Tail.java
@@ -1,4 +1,19 @@
 package lab9.task5;
 
+import lab9.task4.Node;
+
 public class Tail {
+    public static void main(String[] args) {
+        Node head = null;
+        for (int i = 3; i >= 0; i--) {
+            head = new Node(i, head);
+        }
+
+        // Вывод
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
 }
