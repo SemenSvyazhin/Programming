0f5423089cebbc55c416092ddbe6f29446fa3d36
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 19:00:46 +0500

    Update Head.java

--- src/lab9/task5/Head.java
+++ src/lab9/task5/Head.java
@@ -1,4 +1,19 @@
 package lab9.task5;
 
+import lab9.task4.Node;
+
 public class Head {
+    public static void main(String[] args) {
+        Node node3 = new Node(3, null);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
+        Node head = new Node(0, node1);
+
+        // Вывод
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
 }
