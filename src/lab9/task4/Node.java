ca4b86671d581da70fa9b0b144aa27e428ebfac0
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 19:00:03 +0500

    Update Node.java

--- src/lab9/task4/Node.java
+++ src/lab9/task4/Node.java
@@ -1,4 +1,11 @@
 package lab9.task4;
 
 public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
 }
