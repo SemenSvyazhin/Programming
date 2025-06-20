4e40c3842ef0ffaee0e0c2d0eb1a0d29a3ef15fb
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 18:51:11 +0500

    Update Example4.java

--- src/lab9/task1/Example4.java
+++ src/lab9/task1/Example4.java
@@ -1,4 +1,18 @@
 package lab9.task1;
 
 public class Example4 {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+    public static int fact(int n) {
+        int result;
+
+        if (n == 1) {
+            return 1;
+        } else {
+            result = fact(n - 1) * n;
+            return result;
+        }
+    }
 }
